9beefe69cf9711f4d8e908d998c9d7968327ca21
Vyacheslav Melnikov <robber2005@mail.ru>
2026-01-25 21:34:22 +0300

    Greate test_log_in.py

---
+++ tests/test_log_in.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import TestLocators
+import pytest
+
+class TestRegistration:
+    
+    URL = "https://stellarburgers.education-services.ru"
+
+    @pytest.mark.parametrize("entry_point_url,login_locator", [
+        (URL, TestLocators.MAIN_LOGIN_BUTTON), 
+        (URL, TestLocators.HEADER_CABINET_BUTTON), 
+        (f"{URL}/register", TestLocators.REG_FORM_LOGIN_LINK), 
+        (f"{URL}/forgot-password", TestLocators.FORGOT_PASS_LOGIN_LINK)
+        ])
+    
+    def test_login_from_different_pages(self, driver, entry_point_url,login_locator):        
+        driver.get(entry_point_url)
+        driver.find_element(*login_locator).click()
+        assert "/login" in driver.current_url
+        driver.find_element(*TestLocators.LOGIN_EMAIL_INPUT).send_keys("12345678901@mail.ru")
+        driver.find_element(*TestLocators.LOGIN_PASS_INPUT).send_keys("olegoleg1")
+        driver.find_element(*TestLocators.LOGIN_SUBMIT_BUTTON).click()
+        
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(TestLocators.HEADER_CABINET_BUTTON)).click()
+        WebDriverWait(driver, 10).until(EC.url_contains("/account/profile"))
+
+        assert driver.find_element(*TestLocators.PROFILE_LOGIN_INPUT).get_attribute("value") == "12345678901@mail.ru"
