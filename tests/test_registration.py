60e3114fa28bf5fd26ba03fb040ab1721bc796f4
Vyacheslav Melnikov <robber2005@mail.ru>
2026-01-25 21:30:01 +0300

    Greate test_registration.py

---
+++ tests/test_registration.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import TestLocators
+
+class TestRegistration:
+    
+    URL = "https://stellarburgers.education-services.ru"
+
+    def test_registration_success(self, driver):
+        driver.get(f"{self.URL}/register")
+        driver.find_element(*TestLocators.REG_NAME_INPUT).send_keys("IVAN")
+        driver.find_element(*TestLocators.REG_EMAIL_INPUT).send_keys("12345678901@mail.ru")
+        driver.find_element(*TestLocators.REG_PASS_INPUT).send_keys("olegoleg1")
+        driver.find_element(*TestLocators.REG_BUTTON).click()
+        WebDriverWait(driver, 5).until(EC.url_contains("/login"))
+        assert "/login" in driver.current_url
+
+    def test_registration_short_password_error(self, driver):
+        driver.get(f"{self.URL}/register")
+        driver.find_element(*TestLocators.REG_PASS_INPUT).send_keys("12345")
+        driver.find_element(*TestLocators.REG_BUTTON).click()
+        error = driver.find_element(*TestLocators.REG_ERROR_MESSAGE)
+        assert error.text == "Некорректный пароль"
