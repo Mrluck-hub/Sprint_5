4b376c2a0ab9dd532b9a3000a0d480bb553b7625
Vyacheslav Melnikov <robber2005@mail.ru>
2026-01-25 21:46:26 +0300

    Greate test_personal_cabinet_exit.py

---
+++ tests/test_personal_cabinet_exit.py
@@ -0,0 +1,28 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import TestLocators
+
+def test_personal_cabinet(self, driver):
+    driver.get(f"{self.URL}/login")
+    driver.find_element(*TestLocators.LOGIN_EMAIL_INPUT).send_keys("12345678901@mail.ru")
+    driver.find_element(*TestLocators.LOGIN_PASS_INPUT).send_keys("olegoleg1")
+    driver.find_element(*TestLocators.LOGIN_SUBMIT_BUTTON).click()
+    
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(TestLocators.HEADER_CABINET_BUTTON)).click()
+    WebDriverWait(driver, 5).until(EC.url_contains("/account/profile"))
+    assert "/account/profile" in driver.current_url
+
+def test_personal_cabinet_is_exit(self, driver):   
+    driver.get(f"{self.URL}/login")
+    driver.find_element(*TestLocators.LOGIN_EMAIL_INPUT).send_keys("12345678901@mail.ru")
+    driver.find_element(*TestLocators.LOGIN_PASS_INPUT).send_keys("olegoleg1")
+    driver.find_element(*TestLocators.LOGIN_SUBMIT_BUTTON).click()
+    
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(TestLocators.HEADER_CABINET_BUTTON)).click()
+    WebDriverWait(driver, 5).until(EC.url_contains("/account/profile"))
+    
+    driver.find_element(*TestLocators.HEADER_CABINET_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.element_to_be_clickable(TestLocators.PROFILE_EXIT_BUTTON)).click()
+    WebDriverWait(driver, 10).until(EC.url_contains("/login"))
+    assert "/login" in driver.current_url
